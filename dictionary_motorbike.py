542e22d4eb350ec3283e31c262a5beb7c2e72d3f
Manimaran E <manimarane@pc-65-76.customer.ask4.lan>
2020-06-16 13:20:58 +0200

    Another Sample code with usage of Python Dictionary

---
+++ dictionary_motorbike.py
@@ -0,0 +1,4 @@
+bike = {"make": "Honda", "model": "250 dream", "colour": "Red", "engine_size": 250}
+# make, model, colour and engine_size are keys and the related info is the value
+print(bike['engine_size'])
+print(bike['colour'])
