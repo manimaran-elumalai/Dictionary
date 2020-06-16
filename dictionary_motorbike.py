ab5df7a75804602138096b5d36c5b53b47acb516
Manimaran E <manimarane@pc-65-76.customer.ask4.lan>
2020-06-16 16:09:18 +0200

    adding a list to the dictionary with new simple code

--- dictionary_motorbike.py
+++ dictionary_motorbike.py
@@ -2,3 +2,12 @@ bike = {"make": "Honda", "model": "250 dream", "colour": "Red", "engine_size": 2
 # make, model, colour and engine_size are keys and the related info is the value
 print(bike['engine_size'])
 print(bike['colour'])
+
+bike_keys = bike.keys()
+print(bike_keys)
+
+bike["year"] = 2015
+print(bike_keys)
+
+print(bike)
+print(bike.items())
